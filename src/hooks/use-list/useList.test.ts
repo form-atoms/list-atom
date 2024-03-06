c5f858d0816fadb0f1a796fb67878128a35145fb
Miroslav Petrik <miroslavpetrik@outlook.com>
2024-03-06 14:09:26 +0100

    fix(item#moveDown): make item first when last
    Mathew 20:16-20

--- src/hooks/use-list/useList.test.ts
+++ src/hooks/use-list/useList.test.ts
@@ -87,5 +87,35 @@ describe("useListAtom()", () => {
         ],
       });
     });
+
+    describe("when the item is last", () => {
+      it("is moved to the start of the list", async () => {
+        const contacts = listAtom({
+          value: [
+            { email: "primary@contact.com" },
+            { email: "secondary@contact.com" },
+          ],
+          fields: ({ email = "hello@world.com" }) => ({
+            email: fieldAtom({ value: email }),
+          }),
+        });
+        const form = formAtom({ contacts });
+
+        const { result: list } = renderHook(() => useList(contacts));
+        const { result: formSubmit } = renderHook(() => useFormSubmit(form));
+
+        await act(() => list.current.items.at(-1)!.moveDown());
+
+        const onSubmit = vi.fn();
+        await act(() => formSubmit.current(onSubmit)());
+
+        expect(onSubmit).toHaveBeenCalledWith({
+          contacts: [
+            { email: "secondary@contact.com" },
+            { email: "primary@contact.com" },
+          ],
+        });
+      });
+    });
   });
 });
