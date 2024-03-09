0438674dda689ff85f46c4201afcad267ce850d7
Miroslav Petrik <miroslavpetrik@outlook.com>
2024-03-09 18:24:24 +0100

    docs(StoryTitle): make anchorable
    KOTVA

--- .storybook/blocks/StoryTitle.tsx
+++ .storybook/blocks/StoryTitle.tsx
@@ -1,16 +1,14 @@
 import React from "react";
-import { Title } from "@storybook/blocks";
-import { useOf } from "@storybook/blocks";
+import { Markdown, useOf } from "@storybook/blocks";
 import type { ModuleExport } from "@storybook/types";
 
 export const StoryTitle = ({ of }: { of: ModuleExport }) => {
   const resolvedOf = useOf(of || "story", ["story", "meta"]);
   switch (resolvedOf.type) {
     case "story": {
-      return <h3>{resolvedOf.story.name}</h3>;
-    }
-    case "meta": {
-      return <h3>{resolvedOf.preparedMeta.title}</h3>;
+      return <Markdown>{`### ${resolvedOf.story.name}`}</Markdown>;
     }
   }
+
+  return <></>;
 };
