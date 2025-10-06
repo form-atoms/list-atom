96df6c3875aa24a0a652aff5b43694467f6701de
Miroslav Petrik <miroslavpetrik@outlook.com>
2025-10-06 10:37:13 +0200

    docs: reset storybook style for story description, enable pico style

--- .storybook/blocks/Example.tsx
+++ .storybook/blocks/Example.tsx
@@ -1,5 +1,5 @@
-import { PropsWithChildren } from "react";
-import { Story, Description } from "@storybook/addon-docs/blocks";
+import { type PropsWithChildren } from "react";
+import { Story, Markdown } from "@storybook/addon-docs/blocks";
 import type { ModuleExport } from "storybook/internal/types";
 import { StoryTitle } from "./StoryTitle";
 
@@ -7,15 +7,24 @@ export const Example = ({
   of,
   children,
 }: PropsWithChildren<{ of: ModuleExport }>) => {
+  const { description } = of.parameters.docs;
+
   return (
     <>
       <StoryTitle of={of} />
-      <Description of={of} />
+      <Markdown
+        options={{
+          overrides: { code: ({ children }) => <code>{children}</code> },
+        }}
+      >
+        {description.story}
+      </Markdown>
       <article>
         <Story of={of} />
         <footer>
           <details className="code-details">
             <summary>Show code</summary>
+
             {children}
           </details>
         </footer>
