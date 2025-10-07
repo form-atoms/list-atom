f63a7131d25effccd3609d3b3c02e9319073d803
Miroslav Petrik <miroslavpetrik@outlook.com>
2025-10-07 15:12:04 +0200

    chore: bump eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+
+import { defineConfig } from "eslint/config";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+);
