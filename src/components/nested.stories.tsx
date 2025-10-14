8308a15cd21eac46185aa344af986f65c5ddb46e
Miroslav Petrik <miroslavpetrik@outlook.com>
2025-10-14 07:59:49 +0200

    docs(nested): show serialized form data

--- src/components/nested.stories.tsx
+++ src/components/nested.stories.tsx
@@ -75,21 +75,24 @@ export const NestedList = createListStory({
         });
 
         if (result.status === "success") {
-          const { value } = result;
+          const value = {
+            parsed: result.value,
+            serialized: Object.fromEntries(formData.entries()),
+          };
 
           action("parseFormData")(value);
 
           return value;
         } else {
-          return { message: "Failed to parse form data." };
+          return { message: "Failed to parse form data. Fields are required." };
         }
       },
       { message: "" },
     );
 
     const initialValue =
-      "users" in state
-        ? state.users
+      "parsed" in state
+        ? state.parsed.users
         : [
             { name: "Daniel", lastName: "Simons", accounts: [] },
             {
@@ -231,128 +234,19 @@ export const NestedList = createListStory({
         <button type="submit" className="primary">
           Submit form action
         </button>
-        {"users" in state && (
+        {"serialized" in state && (
           <Code className="language-json">
-            {JSON.stringify(state.users, null, 2)}
+            {JSON.stringify(state.serialized, null, 2)}
+          </Code>
+        )}
+        {"parsed" in state && (
+          <Code className="language-json">
+            {JSON.stringify(state.parsed.users, null, 2)}
           </Code>
         )}
       </form>
     );
   },
-  args: {
-    atom: listAtom({
-      name: "users",
-      value: [
-        {
-          name: "Jerry",
-          lastName: "Park",
-          accounts: [{ iban: "SK89 7500 0000 0000 1234 5671" }],
-        },
-      ],
-      fields: () => ({
-        name: fieldAtom({ value: "", name: "name" }),
-        lastName: fieldAtom({ value: "", name: "lastName" }),
-        accounts: listAtom({
-          name: "accounts",
-          value: [],
-          fields: () => ({
-            iban: fieldAtom({ value: "", name: "iban" }),
-          }),
-        }),
-      }),
-    }),
-
-    children: ({ List }) => (
-      <List>
-        <List.Item>
-          {({ fields, index, remove }) => (
-            <article>
-              <header>
-                <nav>
-                  <ul>
-                    <li>
-                      <strong>Person #{index + 1}</strong>
-                    </li>
-                  </ul>
-                  <ul>
-                    <li>
-                      <a
-                        href="#"
-                        role="button"
-                        className="outline secondary"
-                        onClick={(e) => {
-                          e.preventDefault();
-                          remove();
-                        }}
-                      >
-                        Remove
-                      </a>
-                    </li>
-                  </ul>
-                </nav>
-              </header>
-              <div className="grid">
-                <div>
-                  <label>First Name</label>
-                  <InputField
-                    atom={fields.name}
-                    render={(props) => <input {...props} placeholder="Name" />}
-                  />
-                </div>
-                <div>
-                  <label>Last Name</label>
-                  <InputField
-                    atom={fields.lastName}
-                    render={(props) => (
-                      <input {...props} placeholder="Last Name" />
-                    )}
-                  />
-                </div>
-              </div>
-              <List.Nested atom={fields.accounts}>
-                {({ List }) => (
-                  <>
-                    <List.Item>
-                      {({ fields, index, remove }) => (
-                        <>
-                          <label>Account #{index + 1}</label>
-                          <fieldset role="group">
-                            <InputField
-                              atom={fields.iban}
-                              render={(props) => (
-                                <input {...props} placeholder="IBAN" />
-                              )}
-                            />
-                            <RemoveButton remove={remove} />
-                          </fieldset>
-                        </>
-                      )}
-                    </List.Item>
-                    <List.Add>
-                      {({ add }) => (
-                        <button
-                          type="button"
-                          className="outline"
-                          onClick={() => add()}
-                        >
-                          Add Bank Account
-                        </button>
-                      )}
-                    </List.Add>
-                  </>
-                )}
-              </List.Nested>
-            </article>
-          )}
-        </List.Item>
-        <List.Add>
-          {({ add }) => (
-            <button type="button" className="outline" onClick={() => add()}>
-              Add Person
-            </button>
-          )}
-        </List.Add>
-      </List>
-    ),
-  },
+  // @ts-expect-error empty type
+  args: {},
 });
